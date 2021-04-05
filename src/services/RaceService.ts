2841aa2809f3b4b6ea6deb3c7f60a574b1c71dfc
Jesse <jesseniininen@gmail.com>
2021-04-05 20:00:03 +0300

    Add correct api url

--- src/services/RaceService.ts
+++ src/services/RaceService.ts
@@ -1,12 +1,14 @@
 import RaceData from '../types/RaceData';
 import PartialRaceData from '../types/PartialRaceData';
 
+const apiUrl = 'https://f1-results.herokuapp.com';
+
 export async function getRace(id: string): Promise<RaceData> {
-  const res = await fetch('/race/' + id);
+  const res = await fetch(apiUrl + '/race/' + id);
   return res.json();
 }
 
 export async function getAllRaces(): Promise<PartialRaceData[]> {
-  const res = await fetch('/race/');
+  const res = await fetch(apiUrl + '/race/');
   return res.json();
 }
