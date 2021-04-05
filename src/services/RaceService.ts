d153a2b8ff321265bb61f640a502a9fac1525b4a
Jesse <jesseniininen@gmail.com>
2021-04-05 19:55:30 +0300

    Remove hardcoded localhost url

--- src/services/RaceService.ts
+++ src/services/RaceService.ts
@@ -2,11 +2,11 @@ import RaceData from '../types/RaceData';
 import PartialRaceData from '../types/PartialRaceData';
 
 export async function getRace(id: string): Promise<RaceData> {
-  const res = await fetch('http://localhost:3000/race/' + id);
+  const res = await fetch('/race/' + id);
   return res.json();
 }
 
 export async function getAllRaces(): Promise<PartialRaceData[]> {
-  const res = await fetch('http://localhost:3000/race/');
+  const res = await fetch('/race/');
   return res.json();
 }
